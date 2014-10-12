813be31eaaccb271d48c8730c1c37280014f3990
stevebargelt <steve@bargelt.com>
2014-10-12 10:43:17 -0700

    adding sample congif.js

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.jenkins = {};
+
+config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
+config.jenkins.host = "http://username:password@hostname:8080"
+
+module.exports = config;
