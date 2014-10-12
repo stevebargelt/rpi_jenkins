65b27894ebbf05147df13f32f080f1278adf9d02
stevebargelt <steve@bargelt.com>
2014-10-12 10:45:58 -0700

    trying to add a sample config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.jenkins = {};
-
-config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
-config.jenkins.host = "http://username:password@hostname:8080"
-
-module.exports = config;
